c314e191b228eae12b2c5f8a63a93dff293cc495
JeffK25 <A02362908@usu.edu>
2025-09-17 15:59:52 +0000

    Week 4 activities

--- Activities/Week4Activities.py
+++ Activities/Week4Activities.py
@@ -26,3 +26,39 @@ elif year >= 1965:
     print("You are a Gen X")
 elif year >= 1946:
     print("You are a Baby Boomer")
+
+
+
+# Write a program which asks the user their age, then using a while loop displays the year they were born, using the following rules:
+#  - continue the loop while age is greater than 1
+#  - print each time "you were alive in year: " current_year
+#  - decrease age and current_year by one each time
+#  - add an else saying "you were born in year: " current_year
+
+age = int(input("How old are you? "))
+current_year = 2025
+
+while age >= 1:
+    print("You were alive in year: ", current_year)
+    age-=1
+    current_year -=1
+print("You were born in year: ", current_year) 
+
+
+
+# Write a program that prints all the multiples of 5, from 5 to 95 using a for loop. 
+
+number = 5
+for i in range(19):
+    print(number)
+    number +=5
+
+
+
+# Write a program that prints all the multiples of 5, from 5 to 95 using a while loop
+
+number = 5
+
+while number<=95:
+    print(number)
+    number+=5
