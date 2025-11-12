92b3d1be6bbccb9e3f4921ed8ae8c73f62c36a78
JeffK25 <A02362908@usu.edu>
2025-11-12 16:53:46 +0000

    week 12 activities

---
+++ Activities/Week12activities.py
@@ -0,0 +1,30 @@
+
+# Programming Activity 1
+
+# Write a program which asks the user for two numbers, stored in two variables num1 and num2. 
+# Generate a multiplication table for all integers 1 through num1 and 1 through num2. 
+# The bottom right entry in your table should have the product of num1 and num2. 
+# The table must be stored in a 2D list. 
+# The list must be created first with a nested for loop. 
+# Then, the table should be printed by another nested for loop iterating through the 2D list.
+# Steps:
+# - store the variables.
+# - create a list, and add empty lists to the list - however many your need from the variables above.
+# - append the values to the lists.
+# - use a nested for loop to iterate through the list and print the values.
+
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+
+lst = []
+
+for i in range(num1):
+    lst.append([])
+
+for i in range(1,num1+1):
+    for j in range(1, num2+1):
+        lst.append(i*j)
+print(lst)
+
+# for i in range(num1):
+#     for j in range(num2):
